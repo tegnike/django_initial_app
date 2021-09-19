a8a91793590b9e34507422481d3b803607022477
hitoshihasegawa <hitoshi_hasegawa@acceluniverse.com>
2021-09-19 19:23:22 +0200

    pollsアプリの作成/最初のview作成まで

--- myapp/urls.py
+++ myapp/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
+    path('polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
